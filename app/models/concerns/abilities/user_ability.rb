7c72d217fa7d4b7e64455ecfebd493c433f75645
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-03-17 02:54:15 -0400

    Revert "Revert to previous user abiltiy"
    This reverts commit db8083de7599139b05a355aadb9094837f9da5ee.

--- app/models/concerns/abilities/user_ability.rb
+++ app/models/concerns/abilities/user_ability.rb
@@ -7,15 +7,7 @@ module UserAbility
 
       can :crud, Attachment, attachable_type: User.name, attachable_id: authenticable.id
 
-      can [:create, :read, :destroy], Checkin, locatable_type: User.name, locatable_id: authenticable.id
-
-      can :read, Category, company_id: nil
-
-      can [:create, :read, :update], Company, user_id: authenticable.id
-
       can :crud, Device, user_id: authenticable.id
-
-      can [:create, :read], Market, company_id: nil
 
       can :conversations, Message
       can :read, Message, recipient_id: authenticable.id
@@ -28,14 +20,11 @@ module UserAbility
       can :read, Provider, company_id: nil
       can :crud, Provider, user_id: authenticable.id
 
-      can [:read, :update], Task, user_id: authenticable.id
-
       can [:create, :destroy], Token, authenticable_type: User.name, authenticable_id: authenticable.id
 
       can [:read, :update], User, id: authenticable.id
 
-      can :create, WorkOrder, company_id: nil, customer_id: nil, user_id: authenticable.id
-      can [:read, :update], WorkOrder, user_id: authenticable.id
+      can :read, PaperTrail::Version, whodunnit: authenticable.id.to_s
     end
   end
 end

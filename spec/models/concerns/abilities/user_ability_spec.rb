ece3547b3c04f96ab8b124f5502bdc6c8efb5ea3
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-03-22 13:26:11 -0400

    Rename app user ability spec -> user abilty spec

--- spec/models/concerns/abilities/user_ability_spec.rb
+++ spec/models/concerns/abilities/user_ability_spec.rb
@@ -7,41 +7,90 @@ describe UserAbility do
   before { allow_any_instance_of(Ability).to receive(:resource) { resource.class } }
 
   context 'when there is an authenticated user' do
-    context User do
-      let(:resource) { user }
 
+
+        context User do
+          let(:resource) { user }
+
+          it_behaves_like 'ability', :read, :update
+
+          context Attachment do
+            let(:attachment)  {  FactoryGirl.create(:attachment, attachable: user) }
+            let(:resource)    { attachment }
+            it_behaves_like 'ability', :create, :read, :update, :destroy
+          end
+        end
+
+        context Device do
+          let(:resource) { FactoryGirl.create(:device, user: user) }
+          it_behaves_like 'ability', :create, :read, :update, :destroy
+        end
+
+        context Message do
+          subject { Ability.new(ability_user) }
+
+          context 'when the user is the sender' do
+            let(:resource) { FactoryGirl.create(:message, sender: user) }
+            it_behaves_like 'ability', :create, :read, :update, :destroy
+            it { should be_able_to(:conversations, resource) }
+          end
+
+          context 'when the user is the recipient' do
+            let(:resource) { FactoryGirl.create(:message, recipient: user) }
+            it_behaves_like 'ability', :read
+
+            it { should be_able_to(:conversations, resource) }
+
+            it { should_not be_able_to(:create, resource) }
+            it { should_not be_able_to(:update, resource) }
+            it { should_not be_able_to(:destroy, resource) }
+          end
+        end
+
+    context Checkin do
+      let(:resource) { FactoryGirl.create(:checkin, locatable: user) }
+      it_behaves_like 'ability', :create, :read, :destroy
+    end
+
+    context Company do
+      let(:resource) { FactoryGirl.create(:company, user: user) }
       it_behaves_like 'ability', :read, :update
-
-      context Attachment do
-        let(:attachment)  {  FactoryGirl.create(:attachment, attachable: user) }
-        let(:resource)    { attachment }
-        it_behaves_like 'ability', :create, :read, :update, :destroy
-      end
     end
 
-    context Device do
-      let(:resource) { FactoryGirl.create(:device, user: user) }
-      it_behaves_like 'ability', :create, :read, :update, :destroy
-    end
+    context 'when the user is a third-party consumer' do
+      context Provider do
+        let(:provider)  { FactoryGirl.create(:provider) }
+        let(:resource)  { provider }
 
-    context Message do
-      subject { Ability.new(ability_user) }
+        context 'third-party user' do
+          let(:ability_user) { FactoryGirl.create(:user) }
 
-      context 'when the user is the sender' do
-        let(:resource) { FactoryGirl.create(:message, sender: user) }
-        it_behaves_like 'ability', :create, :read, :update, :destroy
-        it { should be_able_to(:conversations, resource) }
-      end
+          context 'provider is not configured to expose availability publicly' do
+            let(:resource) { FactoryGirl.create(:provider) }
 
-      context 'when the user is the recipient' do
-        let(:resource) { FactoryGirl.create(:message, recipient: user) }
-        it_behaves_like 'ability', :read
+            subject { Ability.new(ability_user) }
 
-        it { should be_able_to(:conversations, resource) }
+            it { should_not be_able_to(:index, resource) }
+            it { should_not be_able_to(:read, resource) }
 
-        it { should_not be_able_to(:create, resource) }
-        it { should_not be_able_to(:update, resource) }
-        it { should_not be_able_to(:destroy, resource) }
+            it { should_not be_able_to(:create, resource) }
+            it { should_not be_able_to(:update, resource) }
+            it { should_not be_able_to(:destroy, resource) }
+          end
+
+          context 'provider is configured to expose availability publicly' do
+            let(:resource) { FactoryGirl.create(:provider, publicly_available: true) }
+
+            subject { Ability.new(ability_user) }
+
+            it { should be_able_to(:index, resource) }
+            it { should be_able_to(:read, resource) }
+
+            it { should_not be_able_to(:create, resource) }
+            it { should_not be_able_to(:update, resource) }
+            it { should_not be_able_to(:destroy, resource) }
+          end
+        end
       end
     end
   end

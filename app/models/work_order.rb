b40ae136e6271752ef32074a5cc463f87e1fc5a7
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-04-02 16:03:13 -0400

    Fix invalid reference to wo.id in work order model

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -579,7 +579,7 @@ EOF
     self.config = cfg.with_indifferent_access
     self.save
 
-    Resque.enqueue(FetchContractCreationAddressJob, wo.id, latest_tx_result['id'])
+    Resque.enqueue(FetchContractCreationAddressJob, self.id, latest_tx_result['id'])
   end
 
   def work_order_products_attributes=(work_order_products_attributes)

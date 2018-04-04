95758acdd7c696b123b133d313c95c08ddcc26cf
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-04-03 21:15:38 -0400

    Add abi when watching contract

--- app/jobs/work_orders/fetch_contract_creation_address_job.rb
+++ app/jobs/work_orders/fetch_contract_creation_address_job.rb
@@ -26,9 +26,310 @@ class FetchContractCreationAddressJob
         contract_creation_addr = _trace.select { |_trace| _trace['type'].to_s.match(/^create$/i) }.first['result']['address'] rescue nil
         return unless contract_creation_addr
 
-        BlockchainService.create_contract(jwt, {name: 'UnicornRide', address: contract_creation_addr, network_id: network_id})
+        BlockchainService.create_contract(jwt, {name: 'UnicornRide', address: contract_creation_addr, network_id: network_id, params: {abi: unicorn_ride_abi}})
         work_order.update_attribute(:eth_contract_address, contract_creation_addr)
       end
+    end
+
+    def unicorn_ride_abi
+      <<-EOF
+      [
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "provider",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "peer",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "status",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint8"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "_amount",
+              "type": "uint256"
+            },
+            {
+              "name": "_details",
+              "type": "string"
+            }
+          ],
+          "name": "complete",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "unicorn",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "details",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "identifier",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint128"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "paymentEscrow",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "amount",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [],
+          "name": "completeTransaction",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "unicornWallet",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "_provider",
+              "type": "address"
+            }
+          ],
+          "name": "start",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [],
+          "name": "cancel",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "token",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "name": "_token",
+              "type": "address"
+            },
+            {
+              "name": "_unicorn",
+              "type": "address"
+            },
+            {
+              "name": "_unicornWallet",
+              "type": "address"
+            },
+            {
+              "name": "_paymentEscrow",
+              "type": "address"
+            },
+            {
+              "name": "_peer",
+              "type": "address"
+            },
+            {
+              "name": "_identifier",
+              "type": "uint128"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "name": "_identifier",
+              "type": "uint128"
+            }
+          ],
+          "name": "WorkOrderStarted",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "name": "_identifier",
+              "type": "uint128"
+            }
+          ],
+          "name": "WorkOrderCanceled",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "name": "_identifier",
+              "type": "uint128"
+            },
+            {
+              "indexed": false,
+              "name": "_amount",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "name": "_details",
+              "type": "string"
+            }
+          ],
+          "name": "WorkOrderCompleted",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "name": "_identifier",
+              "type": "uint128"
+            },
+            {
+              "indexed": false,
+              "name": "_paymentAmount",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "name": "feeAmount",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "name": "_details",
+              "type": "string"
+            }
+          ],
+          "name": "TransactionCompleted",
+          "type": "event"
+        }
+      ]
+EOF
     end
   end
 end

e16301934355dc6c510c809ec7c9dcc5caf769e3
Kyle Thomas <kyle@provide.services>
2019-07-26 07:18:02 -0400

    Revert "Drop rvm sourcing from docker entrypoints"
    This reverts commit 84de9c5aef90d7821d1887add1cd981c00acb187.

--- lib/containers/start-rails.sh
+++ lib/containers/start-rails.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+source /etc/profile.d/rvm.sh
+
 sudo service nginx start
 bundle exec rake db:migrate
 exec bundle exec unicorn -E production -c config/unicorn.rb
--- lib/containers/start-resque.sh
+++ lib/containers/start-resque.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+source /etc/profile.d/rvm.sh
 bundle exec rake db:migrate
 exec bundle exec resque-pool --environment production

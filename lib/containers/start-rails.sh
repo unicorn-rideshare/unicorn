5fcc0fad3e1151e2721f9228201ba5ffb8f0f1b7
Kyle Thomas <kyle@provide.services>
2019-07-26 22:44:19 -0400

    Start websocket server prior to starting unicorn app server

--- lib/containers/start-rails.sh
+++ lib/containers/start-rails.sh
@@ -5,4 +5,5 @@ source /etc/profile.d/rvm.sh
 
 sudo service nginx start
 bundle exec rake db:migrate
+bundle exec rake websocket_rails:start_server
 exec bundle exec unicorn -E production -c config/unicorn.rb

e1f127877ceb831c3d76e118da2a1de0866f1cb9
Kyle Thomas <kyle@provide.services>
2019-08-15 03:42:38 -0400

    Read id from newly created stripe accounts

--- app/jobs/stripe/create_stripe_account_job.rb
+++ app/jobs/stripe/create_stripe_account_job.rb
@@ -6,11 +6,15 @@ class CreateStripeAccountJob
       instance = clazz.constantize.unscoped.find(id) rescue nil
       return if instance.try(:stripe_account_id)
 
-      stripe_account_id = Stripe::Account.create({
+      stripe_account = Stripe::Account.create({
         country: 'US', # FIXME-- read country from contactable...
         type: 'custom',
         requested_capabilities: ['transfers', 'card_payments'],
       })
+
+      stripe_account_id = stripe_account.try(:id)
+      raise RuntimeError('Account creation failed') if stripe_account_id.nil?
+
       instance.update_attribute(:stripe_account_id, stripe_account_id)
     end
   end
